4904b739dd4c69d40fdeafcbc7f214675995234f
MoskvichevGeorge <moskvicevg@gmail.com>
2024-11-17 14:57:09 +0300

    Update views.py

--- task2/views.py
+++ task2/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return render(request, 'function_view.html')
+    return render(request, 'second_task/function_view.html')
 
 
 def get(request):
